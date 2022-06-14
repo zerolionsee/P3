9a568f71a7eda8ee69e9874b5cf1bb5cc0413661
zerolionsee <hekun6688@163.com>
2022-06-14 15:51:44 +0800

    6.14

--- hello.c
+++ hello.c
@@ -1,12 +1,13 @@
-// #include <stdio.h>
-// int main()
-// {
-//   int a = 0;
-//   int b = -a;
-//   int c = ~a;
-//   printf("%d\n%d\n", b, c);
-//   printf("%d\n%d\n", sizeof(a), sizeof a);
-// }
+/* #include <stdio.h>
+int main()
+{
+  int a = 0;
+  int b = -a;
+  int c = ~a;
+  printf("%d\n%d\n", b, c);
+  printf("%d\n%d\n", sizeof(a), sizeof a);
+} */
+
 // #define _CRT_SECURE_NO_WARNINGS
 // int print_bin_2(int);
 // #include <stdio.h>
@@ -19,23 +20,23 @@
 //   return 0;
 // }
 
-// #include <stdio.h>
-// int main()
-// {
-//   int a = 3;
-//   int b = 5;
-//   int c = a && b;
-//   printf("%d\n", c);
-// }
+/* #include <stdio.h>
+int main()
+{
+  int a = 3;
+  int b = 5;
+  int c = a && b;
+  printf("%d\n", c);
+} */
 
-// #include <stdio.h>
-// int main()
-// {
-//   unsigned int a = -3;
-//   printf("%u\n", a);//无符号整数打印不出负数
-// }
+/* #include <stdio.h>
+int main()
+{
+  unsigned int a = -3;
+  printf("%u\n", a);//无符号整数打印不出负数
+} */
 
-#include <stdio.h>
+/* #include <stdio.h>
 int main()
 {
   typedef unsigned int u_int;
@@ -43,3 +44,4 @@ int main()
   // u_int a = -3;等价与上面
   printf("%u\n", a);//无符号整数打印不出负数
 }
+ */
